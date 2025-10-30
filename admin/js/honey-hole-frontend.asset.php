bd284ff26bf7da859d904e2008e4d0c6bd57aeb1
Jack Ball <ball.jack2376@gmail.com>
2025-10-30 13:57:51 -0600

    Add - content-visibility CSS to optimize, infinite scroll postponed

--- admin/js/honey-hole-frontend.asset.php
+++ admin/js/honey-hole-frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => 'ebadad86b9240a71cb4e');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '423c3309b9368c3442e6');
